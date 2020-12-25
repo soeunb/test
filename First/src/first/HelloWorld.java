3da9c2e0a0e9f52104f9a7acf09c3a56b8c8a94a
user-pc <user-pc@59.9.145.198>
2020-12-26 03:14:17 +0900

    dev commit
    Conflicts:
    	First/src/first/HelloWorld.java

--- First/src/first/HelloWorld.java
+++ First/src/first/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World");
+<<<<<<< master-dev
 		// dev commit test 2
+=======
+		// dev commit
+>>>>>>> 5a560c7 dev commit
 	}
 
 }
